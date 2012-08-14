4810ecbc250d4ab22fdeca1680f99eb1e18944a7
Joe Groff <arcata@gmail.com>
2012-08-14 11:15:51 -0400

    utilities for parameter packs

--- include/shit/misc.hpp
+++ include/shit/misc.hpp
@@ -35,6 +35,62 @@ namespace shit {
             return f(static_cast<AA>(args)...);
         }
     };
+    
+    template<typename...TT>
+    struct types {};
+    
+    template<typename T, T...NN>
+    struct values {};
+    
+    template<typename T, typename...UU>
+    struct is_type_in;
+    template<typename T, typename U, typename...UU>
+    struct is_type_in<T, U, UU...> : is_type_in<T, UU...> {};
+    template<typename T, typename...UU>
+    struct is_type_in<T, T, UU...> : std::integral_constant<bool, true> {};
+    template<typename T>
+    struct is_type_in<T> : std::integral_constant<bool, false> {};
+
+    template<template<typename> class Trait, typename...TT>
+    struct is_any_type;
+    template<template<typename> class Trait>
+    struct is_any_type<Trait> : std::integral_constant<bool, false> {};
+    template<template<typename> class Trait, typename T, typename...TT>
+    struct is_any_type<Trait, T, TT...>
+    : std::integral_constant<bool, Trait<T>::value || is_any_type<Trait, TT...>::value> {};
+
+    template<template<typename> class Trait, typename...TT>
+    struct is_every_type;
+    template<template<typename> class Trait>
+    struct is_every_type<Trait> : std::integral_constant<bool, true> {};
+    template<template<typename> class Trait, typename T, typename...TT>
+    struct is_every_type<Trait, T, TT...>
+    : std::integral_constant<bool, Trait<T>::value && is_every_type<Trait, TT...>::value> {};
+
+    namespace internal {
+        template<size_t N, typename T, typename...UU>
+        struct type_index;
+    
+        template<size_t N, typename T, typename...UU>
+        struct type_index<N, T, T, UU...> : std::integral_constant<size_t, N> {};
+        
+        template<size_t N, typename T, typename U, typename...UU>
+        struct type_index<N, T, U, UU...> : type_index<N+1, T, UU...> {};
+    }
+    
+    template<typename T, typename...UU>
+    using type_index = internal::type_index<0, T, UU...>;
+    
+    namespace internal {
+        template<size_t N, size_t...NN>
+        struct integers : integers<N-1, N-1, NN...> { };
+        
+        template<size_t...NN>
+        struct integers<0, NN...> { using values = values<size_t, NN...>; };
+    }
+    
+    template<size_t N>
+    using integers = typename internal::integers<N>::values;
 }
 
 #endif
--- test/misc.cpp
+++ test/misc.cpp
@@ -18,6 +18,38 @@ using namespace shit;
 static_assert(is_empty<static_function<void(void*), free>>::value,
               "static_function should be empty");
 
+static_assert(is_type_in<int, char, int>::value,
+              "is_type_in should return true if type present");
+static_assert(!is_type_in<int, char, float>::value,
+              "is_type_in should return false if type not present");
+
+static_assert(type_index<int, int, char>::value == 0,
+              "type_index should return index into type pack");
+static_assert(type_index<int, char, int>::value == 1,
+              "type_index should return index into type pack");
+
+static_assert(is_any_type<is_integral, int, float, void*>::value,
+              "is_any_type should return true if any type fits predicate");
+static_assert(is_any_type<is_integral, int, long, void*>::value,
+              "is_any_type should return true if any type fits predicate");
+static_assert(is_any_type<is_integral, int, long, short>::value,
+              "is_any_type should return true if every type fits predicate");
+static_assert(!is_any_type<is_integral, float, void*, int()>::value,
+              "is_any_type should return false if no type fits predicate");
+static_assert(!is_any_type<is_integral>::value,
+              "is_any_type should return false for empty type pack");
+
+static_assert(!is_every_type<is_integral, int, float, void*>::value,
+              "is_every_type should return true if any type doesn't fit predicate");
+static_assert(!is_every_type<is_integral, int, long, void*>::value,
+              "is_every_type should return false if any type doesn't fit predicate");
+static_assert(is_every_type<is_integral, int, long, short>::value,
+              "is_every_type should return true if every type fits predicate");
+static_assert(!is_every_type<is_integral, float, void*, int()>::value,
+              "is_every_type should return false if no type fits predicate");
+static_assert(is_every_type<is_integral>::value,
+              "is_every_type should return true for empty type pack");
+
 static bool did_free = false;
 namespace {
     void free2(void *x)
